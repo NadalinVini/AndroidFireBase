2ccea29cb923e9bab48eaa3e2ee592f9acaf222f
Vinicius Nadalin <31654995+NadalinVini@users.noreply.github.com>
2018-08-27 11:12:30 -0300

    Second Commit
    2

--- app/src/main/java/com/example/opet/i7/BemVindo.java
+++ app/src/main/java/com/example/opet/i7/BemVindo.java
@@ -2,24 +2,45 @@ package com.example.opet.i7;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import org.w3c.dom.Text;
+
 public class BemVindo extends Activity {
+
+    private Button buttonB;
+    private TextView textView;
+    private Spinner spinner1;
+    private CalendarView Calendar;
+    private EditText TextNome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bem_vindo);
 
+        textView = findViewById(R.id.textView);
+        spinner1 = findViewById(R.id.spinner1);
+        Calendar = findViewById(R.id.Calendar);
+        TextNome = findViewById(R.id.TextNome);
+
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
-
-            String name = user.getDisplayName();
-            String email = user.getEmail();
-            boolean emailVerified = user.isEmailVerified();
-            String uid = user.getUid();
+            textView.setText("Bem vindo, " + user.getEmail());
         }
+    }
+    public void RegistrarDados(View view)
+    {
+        TextNome.getText().toString();
+        spinner1.getSelectedItem();
     }
 }
